aeb900cc262ae4e547b61642481139409378bda2
darriub15 <darriusabrown@gmail.com>
2022-12-03 18:37:30 -0800

    struggling

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getBarber } from './api/homepage'
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 
 function HomePage(accessToken) {
@@ -12,14 +13,15 @@ function HomePage(accessToken) {
       .then((data) => {
         setDatabase(data)
       })
-    storeImages()
   }
 
   const storeImages = () => {
     const imageStorage = {}
-    database.map((barber, index) => {
-      imageStorage[`${index}`] = barber.haircut_images.length
-    })
+
+    for (let i = 0; i < database.length; i++){
+      imageStorage[`${i}`] = 1
+      
+    }
     setImages(imageStorage)
   }
 
@@ -29,6 +31,7 @@ function HomePage(accessToken) {
 
   useEffect(() => {
     getDatabase()
+    storeImages()
   }, [])
 
   if (database.length <= 0) {
@@ -39,7 +42,6 @@ function HomePage(accessToken) {
 
   const renderBarbers = () => {
     return database.map((barber, index) => {
-      console.log(barber.haircut_images[`${index}`])
       return (
         <div className='barber-card'>
           <Card style={{ width: '35rem' }}>
@@ -48,13 +50,14 @@ function HomePage(accessToken) {
               <img className="barber-img" src={barber.profile_image} alt="..." /> {barber.name} 
                 </Card.Title>
             </Card.Body>
-            <Card.Img variant="bottom" src={barber.haircut_images[`${index}`]} />
+            <Card.Img variant="bottom" src={barber.haircut_images[images[index]]} />
             <Card.Body>
               <Card.Text>{barber.description}</Card.Text>
               
-              <button 
-              // onClick={() => {nextImage()}}
-              >Next Image ----> </button>
+              <Button 
+              variant="primary"
+              onClick={() => {nextImage(barber.haircut_images.length)}}
+              >Next Image  </Button>
             </Card.Body>
           </Card>
         </div>
