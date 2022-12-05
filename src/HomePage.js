a5b124b44fc40fd0c2548919913aea75d30e66b9
darriub15 <darriusabrown@gmail.com>
2022-12-04 18:39:36 -0800

    rough draft done

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,27 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { getBarber } from './api/homepage'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
-
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function HomePage(accessToken) {
   const [database, setDatabase] = useState([])
-  const [images, setImages] = useState({})
+  const [images, setImages] = useState(Array(100).fill(0))
 
   const getDatabase = () => {
     getBarber(accessToken)
       .then((data) => {
         setDatabase(data)
       })
-  }
-
-  const storeImages = () => {
-    const imageStorage = Array(2).fill(0)
-
-    for (let i = 0; i < database.length; i++){
-      imageStorage[`${i}`] = 1
-      
-    }
-    setImages(imageStorage)
   }
 
   const nextImage = (imageArrayLength, index) => {
@@ -36,8 +26,7 @@ function HomePage(accessToken) {
 
   useEffect(() => {
     getDatabase()
-    storeImages()
-  }, [])
+  }, )
 
   if (database.length <= 0) {
     return (
@@ -48,23 +37,32 @@ function HomePage(accessToken) {
   const renderBarbers = () => {
     return database.map((barber, index) => {
       return (
-        <div className='barber-card'>
-          <Card style={{ width: '35rem' }}>
-          <Card.Body>
-              <Card.Title>
-              <img className="barber-img" src={barber.profile_image} alt="..." /> {barber.name} 
-                </Card.Title>
-            </Card.Body>
-            <Card.Img variant="bottom" src={barber.haircut_images[images[index]]} />
-            <Card.Body>
-              <Card.Text>{barber.description}</Card.Text>
-              
-              <Button 
-              variant="primary"
-              onClick={() => {nextImage(barber.haircut_images.length, index)}}
-              >Next Image  </Button>
-            </Card.Body>
+        <div className='barber'>
+          <Card className='barber-info' style={{ width: '18rem' }}>
+            <ListGroup variant="flush">
+              <ListGroup.Item className='listgroup'>Price: ${barber.price}</ListGroup.Item>
+              <ListGroup.Item className='listgroup'>Location: {barber.city}, {barber.state}</ListGroup.Item>
+              <ListGroup.Item className='listgroup'>Rating: {barber.ratings}/5</ListGroup.Item>
+            </ListGroup>
           </Card>
+          <div className='barber-card'>
+            <Card style={{ width: '35rem' }}>
+            <Card.Body>
+                <Card.Title>
+                <img className="barber-img" src={barber.profile_image} alt="..." /> {barber.name} 
+                  </Card.Title>
+              </Card.Body>
+              <Card.Img variant="bottom" className='barber-cut' src={barber.haircut_images[images[index]]} />
+              <Card.Body>
+                <Card.Text>{barber.description}</Card.Text>
+                
+                <Button 
+                variant="primary"
+                onClick={() => {nextImage(barber.haircut_images.length, index)}}
+                >Next Image  </Button>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
       )
     })
